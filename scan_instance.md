dd223f1233782fa6adcaa75b0a7ba374acbb2a98
Paul Chang <pchang@cloudpassage.com>
2020-01-27 10:32:24 -0600

    Update Readme with new Jenkins AMI pipeline process

--- scan_instance.md
+++ scan_instance.md
@@ -4,60 +4,68 @@
 
 ## What this does
 
-This pipeline script uses CloudFormation to create a VPC and associated assets
-(security groups, etc) and places one EC2 instance within that VPC, which will
-be based on the AMI ID you configured in the job parameters. Once the instance
-is online, the Halo agent is installed and scanning begins. The Halo agent will
-scan for configuration issues as well as vulnerable packages, and the build
-will pass or fail based on whether or not the number of detected security
-defects exceeds your threshold, as configured in the build parameters.
+This pipeline script is an example of how to use CloudPassage Halo to scan AMIs
+in your AMI build pipeline. Preceding steps of creating an AMI and launching a 
+test instance with the Halo agent installed are outside the scope of CloudPassage
+Halo and are therefore omitted here. Given a CSP instance ID as input, you can
+create a build step that will download and run a Docker container image from a CloudPassage repository.
+The containerized script will get the Software Vulnerability and/or Configuration
+scan results via API and output reports in HTML format. These reports can be viewed
+in Jenkins using third-party plug-ins such as the HTML Publisher plugin
 
-This is provided as example code, which would likely require some adjustment
-before implementing in any end-to-end CI process.
+This is provided as example code, which would likely require some adjustments
+before injecting it into any end-to-end CI process.
 
 ## Requirements
+* CloudPassage access with API key
+* Jenkins with Docker installed on the hosts and added to PATH variable
 
-* CloudPassage portal access
-* Jenkins access
-* Slack plugin configured in Jenkins
-* AWS API keys with privileges sufficient for creating VPCs (and associated
-  assets, like security groups), auto-scaling groups, and EC2 instances.
+## Setup and Usage
 
-## Setup
-
-* AWS
-  * Create API keys with privileges sufficient for using CloudFormation to
-  create VPCs (and associated assets) as well as auto-scaling groups and
-  EC2 instances. Store the key and secret in Jenkins as Username with
-  password-type credentials.
 * CloudPassage:
-  * Log into the CloudPassage portal and create a server group named `Scanner`.
-  * Configure the `Scanner` group with the latest Ubuntu 16.04 CIS policy.
-  * Create a read-only (auditor) API key within the `Scanner` group, and
-   save the key ID and secret in Jenkins as a Username with password-type
-   credential.
-  * Save the Halo agent key for the `Scanner` group in Jenkins as a Secret
-  text-type credential.
+  * Log into the CloudPassage portal and create a server group for AMI testing
+  * Optionally add a configuration policy to the AMI testing group if intending to get
+  configuration scan reports
+  * Create a read-only (auditor) API key within the AMI testing group
+  * Save the Halo agent installation script so that it can be used later in your Jenkins pipeline
 * Jenkins:
-  * Create a Jenkins pipeline task.
-  * Select "This project is parameterized"
-  * Configure the parameters and defaults as illustrated here:
-  [Jenkins scan instance parameters](./images/scan_instance_2.png).
-  * Copy the contents of [scan_instance.groovy](./scripts/scan_instance.groovy)
-  and paste into the pipeline script text box near the bottom of the pipeline
-  configuration page.
-  * Change the credential IDs in the pipeline script to match the IDs for the
-  credentials you created earlier in Jenkins. For instance, the
-  `54458f1e-81a3-4382-9751-e851dadb10b1` in
-  `HALO_CI_API_CREDS = credentials('54458f1e-81a3-4382-9751-e851dadb10b1')` will
-  need to be changed to the ID of the credentials you configured for the Halo
-  API keys.
+  * Install Docker on Jenkins host. Add Docker in Jenkins global tool configuration. 
+  Add Docker to PATH in Jenkins "Configure System"
+  * Save CloudPassage API key and secret in Jenkins as username with password-type credential
+  * Create Jenkins AMI build/bake pipeline
+  * Setup the prerequisite steps of building an AMI and launching a test instance of the
+  newly built AMI with the Halo Agent installed. Optionally, specify the server label in the 
+  Halo agent installation script using the "--server-label=yourLabel" flag. This server
+  label can then be used later on in the containerized script as an alternative method of
+  finding the just-launched test instance in Halo.
+  * Customize and then copy the relevant stages of [scan_instance.groovy](./scripts/scan_instance.groovy)
+  as needed and paste it into your overall AMI pipeline script in the appropriate stages(create if necessary).
+  Make sure that the Instance ID would be available in the stage that would run the Docker
+  container script to retrieve scan results.
+  * If using a freestyle project, you can copy and paste the shell commands instead
+  * Through injecting environment variables to the Docker container, configure security test
+  evaluation criteria such as MAX_CRITICAL, MAX_NON_CRITICAL, and MAX_CVSS. Make sure to
+  specify the scan module (csm or sva). Instance ID, Halo API Secret and Key are required.
+  * Check out [https://github.com/cloudpassage-community/server-ci-helper](https://github.com/cloudpassage-community/server-ci-helper)
+  for more details on the above environment variables and settings
+  * Be sure to mount a host directory onto the container by using the "-v flag" (see in [scan_instance.groovy](./scripts/scan_instance.groovy))
+    that maps to the "/app/reports/html" directory within the container, which is where the
+    security scan reports will be outputted in HTML files.
+  * If a Jenkins host directory is mounted properly, the container script will output
+  "halo_sva_results.html" and/or "halo_csm_results.html" to that host directory. Users
+  can find these reports directly in the Jenkins workspace directory and then viewed using
+  a web browser.
+  * Optionally, a third-party plug-in can be used to publish the HTML scan reports and 
+  made available as a link on the Jenkins build side-panel. In [scan_instance.groovy](./scripts/scan_instance.groovy),
+  the HTML Publisher plugin is used. Specify the host directory containing the report in
+  "reportDir" and the names of the HTML report files as named above. **CloudPassage does not
+  own these third-party plugins and therefore, they are used as-is at the user's own risk.**
+  * Run the build process to get and view the security scan reports that contain test
+  results, vulnerabilities, findings, and remediation information.
 
-## Use
 
-Select "Build with Parameters". You can accept the defaults, or insert your own
-configuration to scan other AMIs available to your Amazon AWS API keys.
-Build results will appear in Slack, and a link to the scan results is clickable
-if further information is required.
+![Jenkins pipeline scan results](./images/halo_sva_results.png)
 
-![Jenkins pipeline scan results](./images/scan_instance_3.png)
+
+![Jenkins pipeline scan results](./images/halo_csm_results.png)
+
--- scripts/scan_instance.groovy
+++ scripts/scan_instance.groovy
@@ -1,166 +1,77 @@
-def HALO_RESULTS = 'UNDEFINED'
-def INSTANCE_MANAGER_IMAGE = 'docker.io/halotools/halo-test-environment'
-def INSTANCE_MANAGER_TAG = 'v0.2'
+// CloudPassage Container images that will retrieve scan results and output scan reports
 def CLOUDPASSAGE_SCANNER_IMAGE = 'docker.io/halotools/server-ci-helper'
 def CLOUDPASSAGE_SCANNER_TAG = 'latest'
-def AWS_ACCESS_KEYS = 'UNDEFINED'
 
 pipeline {
     agent none
     stages {
-        stage('Start Pipeline Build') {
-            steps {
-                 slackSend "Build Started - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-            }
-        }
-        stage('Instantiate test environment') {
+        stage('Initialize workspace') {
             agent any
-            environment {
-                HALO_CI_API_CREDS = credentials('54458f1e-81a3-4382-9751-e851dadb10b1')
-                AWS_ACCESS_KEYS = credentials('387d5882-c2b3-4963-84b4-f0fe533cc104')
-                HALO_AGENT_KEY = credentials('54458f1e-81a3-4382-9751-e851dadb10b1')
-            }
             steps {
-                slackSend "Instantiating testing environment - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-                sh """\
-                docker run -t --rm \
-                    -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEYS_USR}" \
-                    -e "AWS_SECRET_ACCESS_KEY=${AWS_ACCESS_KEYS_PSW}" \
-                    -e "AWS_REGION=${AWS_REGION}" \
-                    -e "ENVIRONMENT_NAME=${ENVIRONMENT_NAME}" \
-                    halotools/halo-test-environment:v0.2 \
-                    deprovision \
-                """
-                sh """\
-                docker run -t --rm \
-                    -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEYS_USR}" \
-                    -e "AWS_SECRET_ACCESS_KEY=${AWS_ACCESS_KEYS_PSW}" \
-                    -e "AWS_SSH_KEY_NAME=${AWS_SSH_KEY_NAME}" \
-                    -e "AWS_REGION=${AWS_REGION}" \
-                    -e "AMI_ID=${AMI_ID}" \
-                    -e "HALO_AGENT_KEY=${HALO_AGENT_KEY}" \
-                    -e "ENVIRONMENT_NAME=${ENVIRONMENT_NAME}" \
-                    -e "HALO_SERVER_LABEL=${ENVIRONMENT_NAME}${BUILD_NUMBER}" \
-                    -e "SERVER_COUNT=1" \
-                    ${INSTANCE_MANAGER_IMAGE}:${INSTANCE_MANAGER_TAG} \
-                    provision \
-                  """
+                sh "mkdir -p ${env.WORKSPACE}/reports/html/"  // Create directory in host workspace to store the HTML scan reports
             }
-            post {
-                failure {
-                    slackSend color: "#FF0000", message: "Failed to instantiate test environment! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-                }
-            }
-        }
-        stage('Scan test instance configuration with Halo') {
-            agent any
-            environment {
-                HALO_CI_API_CREDS = credentials('d7f57353-63e2-4faf-94b0-110644c21770')
-                AWS_ACCESS_KEYS = credentials('387d5882-c2b3-4963-84b4-f0fe533cc104')
-                HALO_RESULTS = "UNKNOWN"
-            }
-            steps {
-                sh """\
-                docker run -t --rm \
-                    -e "HALO_API_KEY=${HALO_CI_API_CREDS_USR}" \
-                    -e "HALO_API_SECRET_KEY=${HALO_CI_API_CREDS_PSW}" \
-                    -e "SERVER_LABEL=${ENVIRONMENT_NAME}${BUILD_NUMBER}" \
-                    -e "SCAN_MODULE=csm" \
-                    -e "MAX_CRITICAL=${MAX_CRITICAL}" \
-                    -e "MAX_NON_CRITICAL=${MAX_NON_CRITICAL}" \
-                    ${CLOUDPASSAGE_SCANNER_IMAGE}:${CLOUDPASSAGE_SCANNER_TAG} \
-                     > ./halo_csm_results.txt \
-                """
-                script {
-                    HALO_RESULTS = sh(returnStdout: true, script: 'cat ./halo_csm_results.txt')
-                }
-              }
-              post {
-                    failure {
-                        slackSend color: "#FF0000", message: "Halo Assessment Failed! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-                        script {
-                            HALO_CSM_RESULTS = sh(returnStdout: true, script: 'cat ./halo_csm_results.txt | egrep -v "^(No findings|Waiting|Server ID)" |head -10')
-                        }
-                        sh 'cat ./halo_csm_results.txt >&2'
-                        slackSend color: "#FF0000", message: "Halo configuration scanner output:\n${HALO_CSM_RESULTS}"
-                        sh """\
-                        docker run -t --rm \
-                            -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEYS_USR}" \
-                            -e "AWS_SECRET_ACCESS_KEY=${AWS_ACCESS_KEYS_PSW}" \
-                            -e "AWS_REGION=${AWS_REGION}" \
-                            -e "ENVIRONMENT_NAME=${ENVIRONMENT_NAME}" \
-                            halotools/halo-test-environment:v0.2 \
-                            deprovision \
-                            """
-                    }
-                }
         }
         stage('Scan test instance for vulnerable packages with Halo') {
             agent any
             environment {
-                HALO_CI_API_CREDS = credentials('d7f57353-63e2-4faf-94b0-110644c21770')
-                AWS_ACCESS_KEYS = credentials('387d5882-c2b3-4963-84b4-f0fe533cc104')
-                HALO_RESULTS = "UNKNOWN"
+                HALO_CI_API_CREDS = credentials('halo-api-creds')  //Get Halo API key and secret stored within Jenkins
             }
             steps {
+                /*
+                Command to run container to retrieve scan results and output HTML scan reports
+                Using the -v flag, mount the host directory to "/app/reports/html" within the container so the outputted reports can be accessed in Jenkins.
+                Pass along environment variables to set test evaluation criteria and scan modules.
+                See https://github.com/cloudpassage-community/server-ci-helper for more details.
+                 */
                 sh """\
-                docker run -t --rm \
+                docker run -t --rm -v /${env.WORKSPACE}/reports/html:/app/reports/html \
                     -e "HALO_API_KEY=${HALO_CI_API_CREDS_USR}" \
                     -e "HALO_API_SECRET_KEY=${HALO_CI_API_CREDS_PSW}" \
-                    -e "SERVER_LABEL=${ENVIRONMENT_NAME}${BUILD_NUMBER}" \
-                    -e "SCAN_MODULE=svm" \
+                    -e "INSTANCE_ID=${INSTANCE_ID}" \
+                    -e "SCAN_MODULE=${SCAN_MODULE}" \
                     -e "MAX_CRITICAL=${MAX_CRITICAL}" \
                     -e "MAX_NON_CRITICAL=${MAX_NON_CRITICAL}" \
+                    -e "MAX_CVSS=${MAX_CVSS}" \
                     ${CLOUDPASSAGE_SCANNER_IMAGE}:${CLOUDPASSAGE_SCANNER_TAG} \
-                     > ./halo_svm_results.txt \
                 """
             }
             post {
                 failure {
-                    slackSend color: "#FF0000", message: "Halo SVM Assessment Failed! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-                    script {
-                        HALO_SVM_RESULTS = sh(returnStdout: true, script: 'cat ./halo_svm_results.txt | egrep -v "^(No findings|Waiting|Server ID)" | head -10')
-                    }
-                    sh 'cat ./halo_svm_results.txt >&2'
-                    slackSend color: "#FF0000", message: "Halo vulnerability scanner output:\n${HALO_SVM_RESULTS}"
-                    sh """\
-                    docker run -t --rm \
-                        -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEYS_USR}" \
-                        -e "AWS_SECRET_ACCESS_KEY=${AWS_ACCESS_KEYS_PSW}" \
-                        -e "AWS_REGION=${AWS_REGION}" \
-                        -e "ENVIRONMENT_NAME=${ENVIRONMENT_NAME}" \
-                        halotools/halo-test-environment:v0.2 \
-                        deprovision \
-                        """
+                    echo "Halo Assessment Failed! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+                    /*
+                    Publish HTML report(s). Point "reportDir" to the host workspace directory where the HTML reports
+                    should have been saved. HTML report files will always be named "halo_sva_results.html" and/or
+                    "halo_csm_results.html"
+                     */
+                    publishHTML (target: [
+                            allowMissing: false,
+                            alwaysLinkToLastBuild: true,
+                            keepAll: true,
+                            reportDir: 'reports/html',
+                            reportFiles: 'halo_sva_results.html, halo_csm_results.html',
+                            reportName: "Cloudpassage AMI Scan Report"
+                    ])
                 }
-            }
-        }
-        stage('Test environment teardown') {
-            agent any
-            environment {
-                HALO_CI_API_CREDS = credentials('d7f57353-63e2-4faf-94b0-110644c21770')
-                AWS_ACCESS_KEYS = credentials('387d5882-c2b3-4963-84b4-f0fe533cc104')
-                HALO_RESULTS = "UNKNOWN"
-            }
-            steps {
-                sh """\
-                docker run -t --rm \
-                    -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEYS_USR}" \
-                    -e "AWS_SECRET_ACCESS_KEY=${AWS_ACCESS_KEYS_PSW}" \
-                    -e "AWS_REGION=${AWS_REGION}" \
-                    -e "ENVIRONMENT_NAME=${ENVIRONMENT_NAME}" \
-                    halotools/halo-test-environment:v0.2 \
-                    deprovision \
-                """
+                success {
+                    echo "Halo Assessment Succeeded! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+                    publishHTML (target: [
+                            allowMissing: false,
+                            alwaysLinkToLastBuild: true,
+                            keepAll: true,
+                            reportDir: 'reports/html',
+                            reportFiles: 'halo_sva_results.html, halo_csm_results.html',
+                            reportName: "Cloudpassage AMI Scan Report"
+                    ])
+                }
             }
         }
     }
     post {
         success {
-            slackSend color: "#00FF00",  message: "AMI ${AMI_ID} scanned clean, build complete! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+            echo "AMI ${INSTANCE_ID} scanned clean, build complete! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
         }
         failure {
-            slackSend color: "#FF0000",  message: "AMI ${AMI_ID} scanned dirty, build failed! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
+            echo "AMI ${INSTANCE_ID} scanned dirty, build failed! - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
         }
     }
 }
